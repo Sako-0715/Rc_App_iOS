d2a7f40480ffff4c2369d74ab5158d9e7535c405
Sako-0715 <sakou.0715@gmail.com>
2024-07-15 17:34:26 +0900

    Initial Commit

---
+++ RcApp(iOS)/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RcApp(iOS)
+//
+//  Created by 酒匂竜也 on 2024/07/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RcApp(iOS)/RcApp_iOS_App.swift
@@ -0,0 +1,17 @@
+//
+//  RcApp_iOS_App.swift
+//  RcApp(iOS)
+//
+//  Created by 酒匂竜也 on 2024/07/15.
+//
+
+import SwiftUI
+
+@main
+struct RcApp_iOS_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
